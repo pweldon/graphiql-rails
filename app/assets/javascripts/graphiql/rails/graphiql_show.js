2bcdb3c666e37f76f50ed0e5306bb02c84018230
Peter Weldon <peter@tausolutions.com.au>
2020-01-09 00:30:11 +0000

    In strict mode code, functions can only be declared at top level or immediately within another function.

--- app/assets/javascripts/graphiql/rails/graphiql_show.js
+++ app/assets/javascripts/graphiql/rails/graphiql_show.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   var graphiqlContainer = document.getElementById("graphiql-container");
   var parameters = {};
 
@@ -24,39 +24,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
         // than present an error.
       }
     }
-    // When the query and variables string is edited, update the URL bar so
-    // that it can be easily shared
-    function onEditQuery(newQuery) {
-      parameters.query = newQuery;
-      updateURL();
-    }
-    function onEditVariables(newVariables) {
-      parameters.variables = newVariables;
-      updateURL();
-    }
-    function updateURL() {
-      var newSearch = '?' + Object.keys(parameters).map(function (key) {
-        return encodeURIComponent(key) + '=' +
-          encodeURIComponent(parameters[key]);
-      }).join('&');
-      history.replaceState(null, null, newSearch);
-    }
-
   }
 
+  // When the query and variables string is edited, update the URL bar so
+  // that it can be easily shared
+  function onEditQuery(newQuery) {
+    parameters.query = newQuery;
+    updateURL();
+  }
+
+  function onEditVariables(newVariables) {
+    parameters.variables = newVariables;
+    updateURL();
+  }
+
+  function updateURL() {
+    var newSearch = '?' + Object.keys(parameters).map(function (key) {
+      return encodeURIComponent(key) + '=' +
+        encodeURIComponent(parameters[key]);
+    }).join('&');
+    history.replaceState(null, null, newSearch);
+  }
 
   // Defines a GraphQL fetcher using the fetch API.
   var graphQLEndpoint = graphiqlContainer.dataset.graphqlEndpointPath;
+
   function graphQLFetcher(graphQLParams) {
     return fetch(graphQLEndpoint, {
       method: 'post',
       headers: JSON.parse(graphiqlContainer.dataset.headers),
       body: JSON.stringify(graphQLParams),
       credentials: 'include',
-    }).then(function(response) {
+    }).then(function (response) {
       try {
         return response.json();
-      } catch(error) {
+      } catch (error) {
         return {
           "status": response.status,
           "message": "The server responded with invalid JSON, this is probably a server-side error",
@@ -76,10 +78,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
   // Render <GraphiQL /> into the body.
-  var element_props = { fetcher: graphQLFetcher, defaultQuery: defaultQuery };
+  var element_props = {fetcher: graphQLFetcher, defaultQuery: defaultQuery};
 
   if (queryParams === 'true') {
-    queryParams = Object.assign({}, queryParams, { query: parameters.query, variables: parameters.variables, onEditQuery: onEditQuery, onEditVariables: onEditVariables });
+    queryParams = Object.assign({}, queryParams, {
+      query: parameters.query,
+      variables: parameters.variables,
+      onEditQuery: onEditQuery,
+      onEditVariables: onEditVariables
+    });
   }
 
   ReactDOM.render(
